5f9b91a617332e859dfaa90d7276e564ac9c0817
Kevin Stone <kevin.stone.silva@gmail.com>
2021-08-24 21:50:05 -0400

    Atualização

--- main.js
+++ main.js
@@ -2,29 +2,34 @@ let qrcode = select("img");
 let qrtext = select("textarea");
 let generateBtn = select("button");
 let downloadBtn = select("a");
-let toast = select("#toast");
+var toast = document.querySelector('#toast');
+
+// let toast = select("#toast");
 
 generateBtn.addEventListener("click", generateQR);
 
 function generateQR() {
-  let size = "1000x1000";
+  let size = "720x720";
   let data = qrtext.value;
   let baseURL = "http://api.qrserver.com/v1/create-qr-code/";
 
   let url = `${baseURL}?data=${data}&size=${size}`;
 
   qrcode.src = url;
+  toastDiv();
   downloadBtn.href = url;
   qrtext.value = "";
-}
-
-function select(el) {
-  return document.querySelector(el);
+  
 }
 
 function toastDiv() {
   toast.className = "show";
   setTimeout( function() {
       toast.className = toast.className.replace("show" , "");
-  },2000);
+  },2200);
+}
+
+
+function select(el) {
+  return document.querySelector(el);
 }

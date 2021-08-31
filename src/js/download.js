084cab8764d5a08264e3ebb9a39258e6736d8631
Kevin Stone <kevin.stone.silva@gmail.com>
2021-08-31 08:17:51 -0400

    Correçao de Bugs

--- src/js/download.js
+++
@@ -1,6 +0,0 @@
-var link = document.createElement("a");
-link.href = 'images.png';
-link.download = 'Download.png';
-document.body.appendChild(link);
-link.click();
-document.body.removeChild(link);
